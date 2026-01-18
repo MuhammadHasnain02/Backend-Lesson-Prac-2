cb1a24c836458ce05c5c10ab70a10b7aa9753b28
MuhammadHasnain02 <mrhasnain9009@gmail.com>
2026-01-18 12:53:03 +0500

    prac

--- index.js
+++ index.js
@@ -115,67 +115,132 @@
 
 // -------------------------------------------------------------
 
+// import http from 'http'
+// import fs from 'fs'
+
+// // Define Port
+// const PORT = 3000
+
+// const server = http.createServer((req , res) => {
+
+//     res.setHeader('Content-Type' , 'text/html')
+
+//     if (req.url === '/') {
+
+//         fs.readFile('./pages/HomePage.html' , 'utf-8' , (err , data) => {
+
+//             if (err) {
+//                 res.end('<h1>Error Loading Home Page</h1>')
+//             }
+//             else {
+//                 res.end(data)
+//             }
+
+//         })
+
+//     }
+//     else if (req.url === '/about') {
+
+//         fs.readFile('./pages/AboutPage.html' , 'utf-8' , (err , data) => {
+
+//             if (err) {
+//                 res.end('<h1>Error Loading About Page</h1>')
+//             }
+//             else {
+//                 res.end(data)
+//             }
+
+//         })
+
+//     }
+//     else if (req.url === '/contact') {
+        
+//         fs.readFile('./pages/ContactPage.html' , 'utf-8' , (err , data) => {
+
+//             if (err) {
+//                 res.end('<h1>Error Loading Contact Page</h1>')
+//             }
+//             else {
+//                 res.end(data)
+//             }
+
+//         })
+
+//     }
+//     else {
+
+//         res.end(`
+//             <h1>404 Page Not Found</h1>
+//             <a href="/">Go Home</a>
+//         `);
+
+//     }
+
+//     console.log(req.url)
+// })
+
+// server.listen(PORT , () => {
+//     console.log(`Server running on port ${PORT}`)
+// })
+
+// -------------------------------------------------------------
+
 import http from 'http'
 import fs from 'fs'
-import dotenv from 'dotenv'
-dotenv.config()
-
 
 // Define Port
-const PORT = process.env.PORT || 3000
+const PORT = 3000
 
 const server = http.createServer((req , res) => {
 
     res.setHeader('Content-Type' , 'text/html')
 
-    if (req.url === '/') {
+    switch (req.url) {
 
-        fs.readFile('./pages/HomePage.html' , 'utf-8' , (err , data) => {
+        case '/':
+            fs.readFile('./pages/HomePage.html' , 'utf-8' , (err , data) => {
 
-            if (err) {
-                res.end('<h1>Error Loading Home Page</h1>')
-            }
-            else {
-                res.end(data)
-            }
+                if (err) {
+                    res.end('<h1>Error Loading Home Page</h1>')
+                }
+                else {
+                    res.end(data)
+                }
 
-        })
+            })
+        break
 
-    }
-    else if (req.url === '/about') {
+        case '/about':
+            fs.readFile('./pages/AboutPage.html' , 'utf-8' , (err , data) => {
 
-        fs.readFile('./pages/AboutPage.html' , 'utf-8' , (err , data) => {
+                if (err) {
+                    res.end('<h1>Error Loading About Page</h1>')
+                }
+                else {
+                    res.end(data)
+                }
 
-            if (err) {
-                res.end('<h1>Error Loading About Page</h1>')
-            }
-            else {
-                res.end(data)
-            }
+            })
+        break
 
-        })
+        case '/contact':
+            fs.readFile('./pages/ContactPage.html' , 'utf-8' , (err , data) => {
 
-    }
-    else if (req.url === '/contact') {
-        
-        fs.readFile('./pages/ContactPage.html' , 'utf-8' , (err , data) => {
+                if (err) {
+                    res.end('<h1>Error Loading Contact Page</h1>')
+                }
+                else {
+                    res.end(data)
+                }
 
-            if (err) {
-                res.end('<h1>Error Loading Contact Page</h1>')
-            }
-            else {
-                res.end(data)
-            }
+            })
+        break
 
-        })
-
-    }
-    else {
-
-        res.end(`
-            <h1>404 Page Not Found</h1>
-            <a href="/">Go Home</a>
-        `);
+        default:
+            res.end(`
+                <h1>404 Page Not Found</h1>
+                <a href="/">Go Home</a>
+            `);
 
     }
 
